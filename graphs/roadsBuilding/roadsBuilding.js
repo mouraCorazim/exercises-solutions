1edfa264fcf9d7cce252f9629904262b1d38d9d8
Lucas Moura Corazim <lucasmouracorazim1@gmail.com>
2021-03-08 22:56:03 +0000

    roadsBuilding

---
+++ graphs/roadsBuilding/roadsBuilding.js
@@ -0,0 +1,36 @@
+function makeSquareMatrixFillWith(length, content){
+  const matrix = []
+
+  for(let i = 0; i < length; i++){
+    matrix[i] = []
+    
+    for(let j = 0; j < length; j++){
+      matrix[i][j] = content
+    }
+  }
+
+  return matrix
+}
+
+module.exports = (cities, roads) => {
+  const matrix = makeSquareMatrixFillWith(cities, 0)
+  const roadsToBuild = []
+  
+  for(let i = 0; i < roads.length; i++){
+    const j = roads[i][0]
+    const k = roads[i][1]
+    
+    matrix[j][k] = 1
+    matrix[k][j] = 1
+  }
+  
+  for(let i = 1; i < cities; i++){
+    for(let j = 0; j < i; j++){
+      if(matrix[i][j] === 0){
+          roadsToBuild.push([j, i])
+      }
+    }
+  }
+    
+  return roadsToBuild
+}
